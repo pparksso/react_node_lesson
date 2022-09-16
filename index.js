24798bee803f3f401e232c972c3bfea72b4dbfde
pparksso <thgml5143@hanmail.net>
2022-09-16 23:03:43 +0900

    express ing

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const env = require("dotenv").config();
+
+app.set("port", process.env.PORT || 8080);
+const PORT = app.get("port");
+app.get("/", (req, res) => {
+  res.send("Hello node");
+});
+app.listen(PORT, () => {
+  console.log(`${PORT}포트`);
+});
